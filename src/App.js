fd0692b8c7b754ad98adeafa5e5e24406ff64d95
Mada Gower <gmgower@gmail.com>
2019-09-18 16:29:45 -0500

    Warp App.js inside ProductContext.Provider.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,25 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import {ProductContext} from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
+    console.log("TCL: App -> data", data)
+    console.log("TCL: App -> products", products)
 	const [cart, setCart] = useState([]);
+    console.log("TCL: App -> setCart", setCart)
+    console.log("TCL: App -> cart", cart)
 
 	const addItem = item => {
 		setCart([...cart, item]);
 	};
 
 	return (
+
 		<div className="App">
+		<ProductContext.Provider value={products}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
@@ -35,6 +44,7 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
